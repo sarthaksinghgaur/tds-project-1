244342f8b2e702ce30a4b91b71244f4ec19d1ca0
Sarthak Singh Gaur <118990586+sarthaksinghgaur@users.noreply.github.com>
2025-02-16 23:59:12 +0530

    Add files via upload
    initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#Hello Snoppy
+#My name is Andrew David Ratnam
+#I'm conventionally concerned useless , however the debate of it being a fallacy is undisputed
+#If you have come this far, you have made an effort to look through and understand
+# May God Bless you and help you in all future endevours
+#Cheers and THE MOST IMPORTANT THING
+# PAY IT FOWARD, Tomrrow California Burrito has an offer Feb 15th anything for 100/- Go enjoy , you have worked hard.
